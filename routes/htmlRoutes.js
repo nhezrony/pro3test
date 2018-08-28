9fcff5e3b91b0fd537a89f217e45dab586d03b59
Noam <noam@Noams-MacBook-Pro.local>
2018-08-28 06:42:59 -0400

    user profile working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,5 +3,6 @@ const public = path.join(__dirname, "../public")
 
 module.exports = function (app) {
     app.get("/", (req, res) => res.sendFile(`${public}/index.html`));
+    app.get("/profile", (req, res) => res.sendFile(`${public}/userProfile.html`));
     // app.get("*", (req, res) => res.sendFile(`${public}/index.html`));
 }
